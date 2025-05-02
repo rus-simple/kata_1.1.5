50f5c935f5a4fe7a3b164d7c89121bb9e3e4f3c1
Sergey <iamrussia@mail.ru>
2025-05-02 18:28:01 +0700

    настройка соединения с БД в классе Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,32 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.SQLException;
+import java.sql.Driver;
+import java.sql.Connection;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+
+    // Статические переменные для хранения настроек подключения к БД
+    private final static String DB_URL = "jdbc:mysql://localhost:3306/mydbtest"; // Адрес базы данных
+    private final static String USER = "admin";                                  // Имя пользователя
+    private final static String PASSWORD = "admin";                              // Пароль
+
+    public static void main(String[] args) {
+        try {
+            // Создаем экземпляр класса драйвера MySQL Connector
+            Driver driver = new com.mysql.cj.jdbc.Driver();
+            // Регистрируем драйвер JDBC
+            DriverManager.registerDriver(driver);
+        } catch (SQLException e) {
+            // Обработка исключений SQL (например, неверное подключение)
+            System.err.println("Не удалось загрузить класс драйвера!");
+        }
+        try(Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD); // Получаем соединение с базой данных используя предоставленные настройки
+            Statement statement = connection.createStatement();) {
+
+        }
+    }
 }
