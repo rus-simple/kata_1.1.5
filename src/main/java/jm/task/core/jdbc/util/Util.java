f56ed80ccade40571b9b325eed9e1d9c651ef6a7
Sergey <iamrussia@mail.ru>
2025-05-02 19:34:06 +0700

    getConnection() class Util готов

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,20 +1,23 @@
 package jm.task.core.jdbc.util;
 
 import java.sql.DriverManager;
-import java.sql.Statement;
+//import java.sql.Statement;
 import java.sql.SQLException;
 import java.sql.Driver;
 import java.sql.Connection;
 
 public class Util {
-    // реализуйте настройку соеденения с БД
+    // реализуйте настройку соединения с БД
 
     // Статические переменные для хранения настроек подключения к БД
-    private final static String DB_URL = "jdbc:mysql://localhost:3306/mydbtest"; // Адрес базы данных
+
+    //private final static String DB_DRIVER = "com.mysql.cj.jdbc.Driver()";                                  // Имя пользователя
+    private final static String DB_URL = "jdbc:mysql://localhost:3306/katadbusers"; // Адрес базы данных
     private final static String USER = "admin";                                  // Имя пользователя
     private final static String PASSWORD = "admin";                              // Пароль
 
-    public static void main(String[] args) {
+    public static Connection getConnection() {
+
         try {
             // Создаем экземпляр класса драйвера MySQL Connector
             Driver driver = new com.mysql.cj.jdbc.Driver();
@@ -24,9 +27,27 @@ public class Util {
             // Обработка исключений SQL (например, неверное подключение)
             System.err.println("Не удалось загрузить класс драйвера!");
         }
-        try(Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD); // Получаем соединение с базой данных используя предоставленные настройки
-            Statement statement = connection.createStatement();) {
 
+        Connection connection = null;
+        try { // Получаем соединение с базой данных используя предоставленные настройки
+
+            // Создаем экземпляр класса драйвера MySQL Connector
+            //Driver driver = new com.mysql.cj.jdbc.Driver();
+            // Регистрируем драйвер JDBC
+            //DriverManager.registerDriver(driver);
+            connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+            if (!connection.isClosed()) {
+                System.out.println("Connection DB open!");   // Если соединение открыто, выводим сообщение
+            }
+        } catch (SQLException e) {
+            System.err.println("Error DB connection!");
         }
+        return connection;
+    }
+    public static void main(String[] args) {
+        // реализуйте алгоритм здесь
+        //Utils util = new Utils();
+        //util.
+        getConnection();
     }
 }
