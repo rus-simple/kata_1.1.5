30a7ae7b538a6438c198c0f39c5f4290623f0538
Sergey <iamrussia@mail.ru>
2025-05-03 16:47:56 +0700

    UserServiceImpl готов

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,40 @@
 package jm.task.core.jdbc.service;
 
+import com.sun.source.util.SourcePositions;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
     public void createUsersTable() {
-
+        createUsersTable();
     }
 
     public void dropUsersTable() {
-
+        dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        saveUser(name, lastName, age);
+        System.out.println("User с именем — " + name + " добавлен в базу данных.");
+        saveUser(name, lastName, age);
+        System.out.println("User с именем — " + name + " добавлен в базу данных.");
+        saveUser(name, lastName, age);
+        System.out.println("User с именем — " + name + " добавлен в базу данных.");
+        saveUser(name, lastName, age);
+        System.out.println("User с именем — " + name + " добавлен в базу данных.");
     }
 
     public void removeUserById(long id) {
-
+        removeUserById(id);
     }
 
     public List<User> getAllUsers() {
+        getAllUsers();
         return null;
     }
 
     public void cleanUsersTable() {
-
+        cleanUsersTable();
     }
 }
