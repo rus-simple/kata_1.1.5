deba9adf8f47053f68ce56c34e9233fcb314fddc
Sergey <iamrussia@mail.ru>
2025-05-03 17:39:17 +0700

    Main class Готов

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,34 @@
 package jm.task.core.jdbc;
 
+import antlr.Utils;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        // Инстанцируем сервис для работы с пользователями
+        UserServiceImpl userService = new UserServiceImpl();
+
+        // Шаг 1: Создаём таблицу
+        userService.createUsersTable();
+        System.out.println("Таблица успешно создана.");
+
+        // Шаг 2: Добавляем пользователя
+        userService.saveUser("Иван", "Иванов", (byte) 30);
+        userService.saveUser("Петр", "Петров", (byte) 20);
+        userService.saveUser("Алексей", "Алексеева", (byte) 25);
+        userService.saveUser("Андрей", "Андреев", (byte) 35);
+        //System.out.println("User с именем — " + name + " добавлен в базу данных.");
+
+        // Шаг 3: Получаем всех пользователей
+        userService.getAllUsers();
+
+        // Шаг 4: Чистим таблицу
+        userService.cleanUsersTable();
+        System.out.println("Таблица успешно очищена.");
+
+        // Шаг 5: Удаляем таблицу
+        userService.dropUsersTable();
+        System.out.println("Таблица успешно удалена.");
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -7,47 +7,24 @@ import java.sql.Driver;
 import java.sql.Connection;
 
 public class Util {
-    // реализуйте настройку соединения с БД
+    // Настройки подключения к базе данных
+    private final static String DB_URL = "jdbc:mysql://localhost:3306/katadbusers";
+    private final static String USER = "admin";
+    private final static String PASSWORD = "admin";
 
-    // Статические переменные для хранения настроек подключения к БД
+    /** * Метод получает соединение с базой данных. */
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, USER, PASSWORD);
+    }
 
-    //private final static String DB_DRIVER = "com.mysql.cj.jdbc.Driver()";                                  // Имя пользователя
-    private final static String DB_URL = "jdbc:mysql://localhost:3306/katadbusers"; // Адрес базы данных
-    private final static String USER = "admin";                                  // Имя пользователя
-    private final static String PASSWORD = "admin";                              // Пароль
-
-    public static Connection getConnection() {
-
+    public static void main(String[] args) {
         try {
-            // Создаем экземпляр класса драйвера MySQL Connector
-            Driver driver = new com.mysql.cj.jdbc.Driver();
-            // Регистрируем драйвер JDBC
-            DriverManager.registerDriver(driver);
-        } catch (SQLException e) {
-            // Обработка исключений SQL (например, неверное подключение)
-            System.err.println("Не удалось загрузить класс драйвера!");
-        }
-
-        Connection connection = null;
-        try { // Получаем соединение с базой данных используя предоставленные настройки
-
-            // Создаем экземпляр класса драйвера MySQL Connector
-            //Driver driver = new com.mysql.cj.jdbc.Driver();
-            // Регистрируем драйвер JDBC
-            //DriverManager.registerDriver(driver);
-            connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
-            if (!connection.isClosed()) {
-                System.out.println("Connection DB open!");   // Если соединение открыто, выводим сообщение
+            Connection conn = getConnection();
+            if (conn != null && !conn.isClosed()) {
+                System.out.println("Подключение к базе данных установлено.");
             }
         } catch (SQLException e) {
-            System.err.println("Error DB connection!");
+            System.err.println("Ошибка подключения к базе данных: " + e.getMessage());
         }
-        return connection;
-    }
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
-        //Utils util = new Utils();
-        //util.
-        getConnection();
     }
 }
